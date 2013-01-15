e0b3946ac2b4efcf2b8ed448bba1ec9526f68b80
Brian Boughton <brian@brianboughton.com>
2013-01-15 10:11:09 -0700

    Create osx setup script

---
+++ bin/osx-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+###############################################################################
+# Mail                                                                        #
+###############################################################################
+
+# Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
+defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\\U21a9"
