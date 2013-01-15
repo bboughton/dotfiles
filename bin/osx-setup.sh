eeb215a23f4be0f1571d50e5433acefbb3294d9a
Brian Boughton <brian@brianboughton.com>
2013-01-15 10:55:02 -0700

    Add spotify setting

--- bin/osx-setup.sh
+++ bin/osx-setup.sh
@@ -7,3 +7,10 @@
 
 # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
 defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\\U21a9"
+
+###############################################################################
+# Spotify                                                                     #
+###############################################################################
+
+# Add the keyboard shortcut ⌘ + 1 to open client
+defaults write com.spotify.client NSUserKeyEquivalents -dict-add "Spotify" "@1"
