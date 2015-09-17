1272da0a8bc85571230e25560fd06a86b8235c2d
Brian Boughton <brian@brianboughton.com>
2015-09-17 09:22:24 -0700

    Update trash cleaner
    Enable logging of trash agent
    
    Change clean script to remove files that haven't been modified within
    the last 30 days

--- scripts/srm-trash-older-then-30-days.sh
+++ scripts/srm-trash-older-then-30-days.sh
@@ -14,9 +14,9 @@ err() {
 
 log "Cleaning trash"
 
-# Find files in the local trash that are older then 30 days
+# Find files in the local trash that haven't been modified in the last 30 days
 # Securely remove all the old files and directories with 7 passes
 #
 # Using -mindepth 1 -prune, so that if a DIRECTORY is in the Trash every file
 # in the directory is deleted (good for app bundles)
-/usr/bin/find $HOME/.Trash -mindepth 1 -prune -not -newerct '30 days ago' -exec /usr/bin/srm --recursive --medium --zero --force {} \;
+/usr/bin/find $HOME/.Trash -mindepth 1 -prune -not -newermt '30 days ago' -exec /usr/bin/srm --recursive --medium --zero --force {} \;
