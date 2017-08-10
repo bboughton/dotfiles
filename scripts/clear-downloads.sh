7b6316d97a5e268a78b889a334006346f59bd224
Brian Boughton <brian@brianboughton.com>
2017-08-10 08:57:29 -0700

    Fix bug
    Fix bug where a space in a file name would prevent the proper execution
    of this script.

--- scripts/clear-downloads.sh
+++ scripts/clear-downloads.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-
-/usr/bin/find $HOME/Downloads -mindepth 1 -prune -not -newerct '1 week ago' | xargs -J % /bin/mv % ~/.Trash
+/usr/bin/find $HOME/Downloads -mindepth 1 -prune -not -newerct '1 week ago' | while read item
+do
+	/bin/mv "${item}" ~/.Trash
+done
